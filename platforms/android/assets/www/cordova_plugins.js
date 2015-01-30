5d78de40a1d2f0afee83ab38fe17c30edda10738
Isaac Burns <isaacburns@gmail.com>
2015-01-30 10:52:28 -0600

    add org.apache.cordova.console plugin

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -2,6 +2,8 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "org.apache.cordova.console": "0.2.12"
+}
 // BOTTOM OF METADATA
 });
