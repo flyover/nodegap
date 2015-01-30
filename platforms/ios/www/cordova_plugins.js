2f0632d313e380291288a5521fbf9cd0d4e89164
Isaac Burns <isaacburns@gmail.com>
2015-01-30 10:53:51 -0600

    add org.apache.cordova.device plugin

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -13,12 +13,20 @@ module.exports = [
         "clobbers": [
             "cordova.logger"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.apache.cordova.console": "0.2.12"
+    "org.apache.cordova.console": "0.2.12",
+    "org.apache.cordova.device": "0.2.13"
 }
 // BOTTOM OF METADATA
 });
