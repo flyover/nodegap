a87e8c3539049ee0d1a5562b319e37c1bbb3b02c
Isaac Burns <isaacburns@gmail.com>
2015-01-30 14:30:04 -0600

    use ace editor for source and output

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,71 +1,223 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+/**
+ * Copyright (c) Flyover Games, LLC
  */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+var boot = function ()
+{
+///	document.addEventListener('deviceready', function ()
+///	{
+///		var id = 'deviceready';
+///		var parentElement = document.getElementById(id);
+///		var listeningElement = parentElement.querySelector('.listening');
+///		var receivedElement = parentElement.querySelector('.received');
+///
+///		listeningElement.setAttribute('style', 'display:none;');
+///		receivedElement.setAttribute('style', 'display:block;');
+///
+///		console.log('Received Event: ' + id);
+///
+///		NodePlugin.loop(
+///			function (str)
+///			{
+///				console.log("stdout", str);
+///				receivedElement.innerHTML = str;
+///			},
+///			function (str)
+///			{
+///				console.log("stderr", str);
+///			}
+///		);
+///
+///		setTimeout(function ()
+///		{
+///			NodePlugin.exec("console.log('Hello, World!');", function (err)
+///			{
+///				console.log("done", err);
+///			});
+///		}, 3000);
+///
+///	}, false);
 
-        console.log('Received Event: ' + id);
+///	var app = {
+/// 	// Application Constructor
+/// 	initialize: function() {
+/// 		this.bindEvents();
+/// 	},
+/// 	// Bind Event Listeners
+/// 	//
+/// 	// Bind any events that are required on startup. Common events are:
+/// 	// 'load', 'deviceready', 'offline', and 'online'.
+/// 	bindEvents: function() {
+/// 		document.addEventListener('deviceready', this.onDeviceReady, false);
+/// 	},
+/// 	// deviceready Event Handler
+/// 	//
+/// 	// The scope of 'this' is the event. In order to call the 'receivedEvent'
+/// 	// function, we must explicitly call 'app.receivedEvent(...);'
+/// 	onDeviceReady: function() {
+/// 		app.receivedEvent('deviceready');
+/// 	},
+/// 	// Update DOM on a Received Event
+/// 	receivedEvent: function(id) {
+/// 		var parentElement = document.getElementById(id);
+/// 		var listeningElement = parentElement.querySelector('.listening');
+/// 		var receivedElement = parentElement.querySelector('.received');
+///	
+/// 		listeningElement.setAttribute('style', 'display:none;');
+/// 		receivedElement.setAttribute('style', 'display:block;');
+///	
+/// 		console.log('Received Event: ' + id);
+///	
+/// 		NodePlugin.loop(
+/// 			function (str)
+/// 			{
+/// 				console.log("stdout", str);
+/// 				receivedElement.innerHTML = str;
+/// 			},
+/// 			function (str)
+/// 			{
+/// 				console.log("stderr", str);
+/// 			}
+/// 		);
+///	
+/// 		setTimeout(function ()
+/// 		{
+/// 			NodePlugin.exec("console.log('Hello, World!');", function (err)
+/// 			{
+/// 				console.log("done", err);
+/// 			});
+/// 		}, 3000);
+/// 	}
+///	};
+///	
+///	app.initialize();
 
-        NodePlugin.loop(
-            function (str)
-            {
-                console.log("stdout", str);
-                receivedElement.innerHTML = str;
-            },
-            function (str)
-            {
-                console.log("stderr", str);
-            }
-        );
+	document.body.style.width = '100%';
+	document.body.style.height = '100%';
+	document.body.style.border = '0px';
+	document.body.style.margin = '0px';
+	document.body.style.padding = '0px';
+	document.body.style['box-sizing'] = 'border-box';
+	document.body.style['-moz-box-sizing'] = 'border-box';
+	document.body.style['-webkit-box-sizing'] = 'border-box';
+	document.body.style.backgroundColor = '#46483e';
 
-        setTimeout(function ()
-        {
-            NodePlugin.exec("console.log('Hello, World!');", function (err)
-            {
-                console.log("done", err);
-            });
-        }, 3000);
-    }
-};
+	var table_div = document.body.appendChild(document.createElement('div'));
+	table_div.style.position = 'absolute';
+	table_div.style.display = 'table';
+	table_div.style.width = '100%';
+	table_div.style.height = '100%';
 
-app.initialize();
+	var default_source = 
+	[
+		"console.log(\"Hello, World!\");",
+		""
+	];
+
+	//var div = table_div.appendChild(document.createElement('div'));
+	//var source_textarea = div.appendChild(document.createElement('textarea'));
+	//source_textarea.style.width = '100%';
+	//source_textarea.rows = 12;
+	//source_textarea.style.backgroundColor = 'rgba(255,255,255,0.75)';
+	//source_textarea.innerHTML = default_source.join('\n');
+
+	var table_row_div = table_div.appendChild(document.createElement('div'));
+	table_row_div.style.display = 'table-row';
+	table_row_div.style.width = '100%';
+	//table_row_div.style.height = '100%';
+	table_row_div.style.background = "url(img/logo.png) no-repeat center center";
+	var source_editor_div = table_row_div.appendChild(document.createElement('div'));
+	source_editor_div.id = 'source_editor';
+	source_editor_div.style.width = '100%';
+	source_editor_div.style.height = '100%';
+	source_editor_div.style.backgroundColor = 'rgba(255,255,255,0.65)';
+	// create ace source editor
+	var source_editor = ace.edit('source_editor');
+	source_editor.setValue("");
+	source_editor.insert(default_source.join('\n'));
+	source_editor.renderer.setShowGutter(false);
+	source_editor.renderer.setShowPrintMargin(false);
+	// set JavaScript mode
+	var JavaScriptMode = require("ace/mode/javascript").Mode;
+	source_editor.getSession().setMode(new JavaScriptMode());
+
+	var table_row_div = table_div.appendChild(document.createElement('div'));
+	table_row_div.style.display = 'table-row';
+	table_row_div.style.width = '100%';
+	table_row_div.style.height = '0';
+	table_row_div.style.textAlign = 'center';
+	var exec_button = table_row_div.appendChild(document.createElement('button'));
+	exec_button.innerHTML = "RUN";
+	exec_button.style.margin = '8px';
+	exec_button.style.padding = '8px';
+
+	//var div = table_div.appendChild(document.createElement('div'));
+	//var output_textarea = div.appendChild(document.createElement('textarea'));
+	//output_textarea.style.width = '100%';
+	//output_textarea.rows = 8;
+	//output_textarea.style.backgroundColor = 'rgba(255,255,255,0.75)';
+	//output_textarea.innerHTML = "";
+
+	var table_row_div = table_div.appendChild(document.createElement('div'));
+	table_row_div.style.display = 'table-row';
+	table_row_div.style.width = '100%';
+	table_row_div.style.height = '25%';
+	var output_editor_div = table_row_div.appendChild(document.createElement('div'));
+	output_editor_div.id = 'output_editor';
+	output_editor_div.style.width = '100%';
+	output_editor_div.style.height = '100%';
+	output_editor_div.style.backgroundColor = 'rgba(255,255,255,0.65)';
+	// create ace output editor
+	var output_editor = ace.edit('output_editor');
+	output_editor.setValue("");
+	output_editor.setReadOnly(true);
+	output_editor.setHighlightActiveLine(false);
+	output_editor.renderer.setShowGutter(false);
+	output_editor.renderer.setShowPrintMargin(false);
+
+	exec_button.addEventListener('click', function (evt)
+	{
+		exec_button.disabled = true;
+		//output_textarea.innerHTML = ""; // clear output
+		output_editor.setValue(""); // clear output
+		//var code = source_textarea.value;
+		var code = source_editor.getValue();
+		NodePlugin.exec(code, function (err)
+		{
+			//exec_button.disabled = false;
+			setTimeout(function () { exec_button.disabled = false; }, 1000);
+		});
+	}, false);
+
+	var xhr = new XMLHttpRequest();
+	xhr.addEventListener('readystatechange', function (event)
+	{
+		if ((this.readyState == 4) && ((this.status == 200) || (this.status == 0)))
+		{
+			//source_textarea.innerHTML = this.responseText;
+			source_editor.setValue("");
+			source_editor.insert(this.responseText);
+		}
+	});
+	xhr.open("GET", "default.js", true);
+	xhr.send();
+
+	exec_button.disabled = true;
+	document.addEventListener('deviceready', function ()
+	{
+		NodePlugin.loop(
+			function (str)
+			{
+				//output_textarea.innerHTML += str;
+				output_editor.insert(str);
+			},
+			function (str)
+			{
+				//output_textarea.innerHTML += str;
+				output_editor.insert(str);
+			}
+		);
+		//exec_button.disabled = false;
+		setTimeout(function () { exec_button.disabled = false; }, 1000);
+	}, false);
+}
